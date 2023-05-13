824908d55090a26caf6d5c59bf302768b37a5544
Abdul Rahman <64832120+Abdul-43@users.noreply.github.com>
2023-05-13 22:30:22 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,15 @@
-let arr=["madam","dad","master","abdul","MOM"];
-let palindrome=arr.filter(function(str){
-        let temp=str;
-        var res="";
-        for(var i=0;i<str.length;i++){
-            var res=str.charAt(i)+res;
-        }
-        if(temp===res){
-            return true
-        }
-    })
-
+let arr = ["madam", "dad", "master", "abdul", "MOM"];
+let palindrome = (function () {
+  return arr.filter(function (str) {
+    let temp = str;
+    var res = "";
+    for (var i = 0; i < str.length; i++) {
+      var res = str.charAt(i) + res;
+    }
+    if (temp === res) {
+      return true;
+    }
+  });
+})();
 
 console.log(palindrome);
