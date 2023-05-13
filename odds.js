9267293ee1da5f65c9f8011a2b931b9a2afe6af5
Abdul Rahman <64832120+Abdul-43@users.noreply.github.com>
2023-05-13 22:28:08 +0530

    Update odds.js

--- odds.js
+++ odds.js
@@ -1,13 +1,15 @@
 let arr = [1,2,3,4,5,6,7,8,9,10]
 
 // let odds = arr.filter(n => n%2)
-
+let res=[]
 // console.log(odds)
-let odd=function (arr){
+let odd= (arr)=>{
     for(let x of arr){
         if(x%2!=0){
-            var[]= x;
+            res.push(x)
+            
         }
     }
+    return res
 }
 console.log(odd(arr));
